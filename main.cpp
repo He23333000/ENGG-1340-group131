cccb03ac4abcb0e18c3d8279ca06ef1901f56e7a
He23333000 <99416618+He23333000@users.noreply.github.com>
2022-05-06 20:29:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,29 @@
 
 int main()
 {
+	game_data game(int bet_amount);
+	{
+	ifstream fin;
+	string file2="textinput1.txt";
+	fin.open(file2);
 	Bet bet;
 	game_data data;
 	cout<<string(35,'*')<<" Welcome to Blackjack game "<<setw(13)<<string(35,'*')<<endl;
 	
-	vector<string> win;
-	string file_name;
-	cout<<"input the file name for bet record: ";
-	cin>>file_name;
+	vector<string> win;//for storing winner in each round
 	
-	while (file_name.substr(file_name.find_last_of('.')+1) != "txt")
+	string file_name;
+	cout<<"input the output file name for bet record: ";
+	fin>>file_name;
+	cout<<file_name<<endl;
+	
+	while (file_name.substr(file_name.find_last_of('.')+1) != "txt") //error for file
 	{
-		cout<<"invalid file name!"<<endl<<"re-enter the file name: ";
-		cin>>file_name;
+		cout<<"invalid file name!"<<endl<<"re-enter the output file name: ";
+		fin>>file_name;
+		cout<<file_name<<endl;
 	}
+	cout<<endl;
 	
 	ofstream fout;
 	fout.open(file_name);
@@ -26,25 +35,31 @@ int main()
 	}
 	
 	cout<<"Enter the total bet for tonight: ";
-	cin>>bet.initial_bet;
+	fin>>bet.initial_bet;
+	cout<<bet.initial_bet<<endl<<endl;
+	cout<<string(15,'*')<<" GAME STARTS "<<setw(13)<<string(15,'*')<<endl;
+	cout<<endl;
 	
 	int round=1;
 	double bet_amount;
 	bet.current_bet=bet.initial_bet;
-	while (true)
+	while (true) //error for bet
 	{
 		cout<<"bet for round "<<round<<": ";
 		round++;
-		cin>>bet_amount;
+		fin>>bet_amount;
+		cout<<bet_amount<<endl;
 		
 		if (bet.current_bet<bet_amount)
 		{
 			cout<<"Invalid bet amount"<<endl;
 			cout<<"re-enter bet amout again: ";
-			cin>>bet_amount;
+			fin>>bet_amount;
+			cout<<bet_amount<<endl;
 		}
+		cout<<string(30,'-')<<endl; 
 		
-		data=game(bet_amount);
+		data=game(bet_amount); //update the struct in each round
 		cout<<"Your original bet: "<<bet.current_bet<<endl;
 		bet.bet_change=data.bankroll_change;
 		if (data.game_result=="Dealer") bet.current_bet-=bet.bet_change;
@@ -64,55 +79,94 @@ int main()
 		string command;
 		cout<<"input command (continue/show record/quit): ";
 		
-		getline(cin>>ws,command);
+		getline(fin>>ws,command); //input command with getline
+		cout<<command<<endl;
 		
 		while (command!="quit" && command!="show record" && command!="continue")
 		{
 			cout<<"Invalid. Re-enter the command: ";
-			getline(cin>>ws,command);
+			getline(fin>>ws,command);
+			cout<<command<<endl;
 		}
 		cout<<endl;
-		
-		if (command=="continue")
+		int determination_of_break=0;
+		while (true)
 		{
-			cout<<endl;
-			cout<<string(35,'*')<<" New turn "<<setw(13)<<string(35,'*')<<endl;
-			cout<<endl;
-		}
-		
-		else if (command=="show record")
-		{
-			ifstream fin;
-			fin.open(file_name);
-			string word;
-			double a,b,c;
-			int cnt=1;
-			cout<<"The Total_bet = "<<bet.initial_bet<<endl;
-			cout<<string(20,'-')<<endl;
 			
-			while (getline(fin,word))
+			if (command=="continue")
 			{
-				
-				if (word=="") break;
-				istringstream num(word);
-				num>>b>>c;
-				cout<<"Round "<<cnt<<": "<<"winner is "<<((win[cnt-1]=="Draw")?"No one":win[cnt-1])<<endl;
-				cout<<setw(14)<<right<<"Previous bet: "<<fixed<<setprecision(2)<<b+c<<endl;
-				cout<<setw(14)<<right<<"Bet change: "<<fixed<<setprecision(2)<<c<<endl;
-				cout<<setw(14)<<right<<"Current bet: "<<fixed<<setprecision(2)<<b<<endl;
-				cout<<string(20,'-')<<endl;
-				cnt++;
+				cout<<endl;
+				cout<<string(35,'*')<<" New turn "<<setw(13)<<string(35,'*')<<endl;
+				cout<<endl;
+				break;
 			}
-			cout<<string(35,'*')<<" New turn "<<setw(13)<<string(35,'*')<<endl;
 			
-		}
-		else if (command=="quit")
-		{
-			cout<<"Thanks for comming, your left bet is: $"<<bet.current_bet;
-			break;
-		}
+			else if (command=="show record")
+			{
+				ifstream input;
+				input.open(file_name);
+				string word;
+				double a,b,c;
+				int cnt=1;
+				cout<<"The Total_bet = "<<bet.initial_bet<<endl;
+				cout<<string(20,'-')<<endl;
+				
+				while (getline(input,word))
+				{
+					
+					if (word=="") break;
+					istringstream num(word);
+					num>>b>>c;
+					cout<<"Round "<<cnt<<": "<<"winner is "<<((win[cnt-1]=="Draw")?"No one":win[cnt-1])<<endl;
+					double previous;
+					cout<<setw(14)<<right<<"Previous bet: ";
+					if (win[cnt-1]=="Dealer" || win[cnt-1]=="Draw") previous=b+c;
+					else if (win[cnt-1]=="Player") previous=b-c;
+					cout<<fixed<<setprecision(2)<<previous<<endl;
+					cout<<setw(14)<<right<<"Bet change: "<<fixed<<setprecision(2)<<c<<endl;
+					cout<<setw(14)<<right<<"Current bet: "<<fixed<<setprecision(2)<<b<<endl;
+					cout<<string(20,'-')<<endl;
+					cnt++;
+				}
+				cout<<"input command (continue/show record/quit): ";
 		
+				getline(fin>>ws,command);
+				cout<<command<<endl;
+				
+				while (command!="quit" && command!="show record" && command!="continue")
+				{
+					cout<<"Invalid. Re-enter the command: ";
+					getline(fin>>ws,command);
+					cout<<command<<endl;
+				}
+				cout<<endl;
+				if (command=="continue")
+				{
+					cout<<string(35,'*')<<" New turn "<<setw(13)<<string(35,'*')<<endl;
+					cout<<endl;
+					break;
+				}
+				else if (command=="quit")
+				{
+					cout<<"Thanks for comming, your left bet is: $"<<bet.current_bet;
+					determination_of_break=1;				
+					break;
+				}
+				
+				
+			}
 			
+			else if (command=="quit")
+			{
+				cout<<"Thanks for comming, your left bet is: $"<<bet.current_bet;
+				break;
+			}
+			if (determination_of_break==1) break;
+		}
+			
+		if (determination_of_break==1) break;
 	}
+	
+}
 	
 }
